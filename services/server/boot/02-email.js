78a79ac051ccf62d67387f02081eee0c19bdae5e
Raphael Varonos <r.varonos@gmail.com>
2017-07-01 16:30:12 +0300

    Load project email data

--- services/server/boot/02-email.js
+++ services/server/boot/02-email.js
@@ -9,7 +9,12 @@ var htmlToText = require('html-to-text');
 
 module.exports = function(app) {
 
+    var project = app.get('options');
+    project = project.web || project.client;
+    project = project.project;
+
     var email = app.get('email');
+
     if (!email) {
         email = {};
         app.set('email', email);
@@ -30,66 +35,77 @@ module.exports = function(app) {
     // Load Email Templates
     //////////////////////////////////////////////////////////////////////
 
-    var pathTemplates = path.join(__dirname, '../../templates');
-    var templateDirs = fs.readdirSync(pathTemplates);
-    var dataMain = yaml.safeLoad(fs.readFileSync(path.join(pathTemplates, '_data.yml'), 'utf8'));
+    var dataMain = {};
+    var templatePaths = [
+        path.join(__dirname, '../../templates'),
+        path.join(project.paths.project, 'email')
+    ];
 
-    var helpers = {
-        partial: function(path_partial, data) {
+    return Promise.map(templatePaths, function(pathTemplates) {
 
-            var file_path = path.join(pathTemplates, path_partial + '.ejs');
-            var file_content = fs.readFileSync(file_path, 'utf8');
+            var templateDirs = fs.readdirSync(pathTemplates);
 
-            data = _.extend({}, this, data);
+            _.merge(dataMain,
+                yaml.safeLoad(fs.readFileSync(path.join(pathTemplates, '_data.yml'), 'utf8'))
+            );
 
-            return ejs.render.apply(this, [file_content, data]);
-        },
-        lng: function(obj) {
-            if (!obj) {
-                return;
-            }
-            if (_.isString(obj)) {
-                return obj;
-            }
-            return obj[this.language] || obj.en || obj.gr;
-        }
-    };
+            var helpers = {
+                partial: function(path_partial, data) {
 
-    return Promise.map(templateDirs, function(templateDir) {
+                    var file_path = path.join(pathTemplates, path_partial + '.ejs');
+                    var file_content = fs.readFileSync(file_path, 'utf8');
 
-            var pathTemplate = path.join(pathTemplates, templateDir);
+                    data = _.extend({}, this, data);
 
-            var stats = fs.statSync(pathTemplate);
-
-            if (!stats.isDirectory()) {
-                return;
-            }
-
-            if (templateDir[0] == '_') {
-                return;
-            }
-
-            var templateData = yaml.safeLoad(fs.readFileSync(path.join(pathTemplate, 'data.yml'), 'utf8'));
-            var renderer = new EmailTemplate(pathTemplate);
-
-            templates[templateDir] = {
-                renderer: renderer,
-                data: templateData,
-                render: function(data) {
-
-                    return new Promise(function(resolve, reject) {
-                        renderer.render(
-                            _.extend({}, dataMain, templateData, data, helpers),
-                            function(err, result) {
-                                if (err) {
-                                    return reject(err);
-                                }
-                                resolve(result);
-                            });
-                    });
+                    return ejs.render.apply(this, [file_content, data]);
+                },
+                lng: function(obj) {
+                    if (!obj) {
+                        return;
+                    }
+                    if (_.isString(obj)) {
+                        return obj;
+                    }
+                    return obj[this.language] || obj.en || obj.gr;
                 }
             };
 
+            return Promise.map(templateDirs, function(templateDir) {
+
+                var pathTemplate = path.join(pathTemplates, templateDir);
+
+                var stats = fs.statSync(pathTemplate);
+
+                if (!stats.isDirectory()) {
+                    return;
+                }
+
+                if (templateDir[0] == '_') {
+                    return;
+                }
+
+                var templateData = yaml.safeLoad(fs.readFileSync(path.join(pathTemplate, 'data.yml'), 'utf8'));
+                var renderer = new EmailTemplate(pathTemplate);
+
+                templates[templateDir] = {
+                    renderer: renderer,
+                    data: templateData,
+                    render: function(data) {
+
+                        return new Promise(function(resolve, reject) {
+                            renderer.render(
+                                _.extend({}, dataMain, templateData, data, helpers),
+                                function(err, result) {
+                                    if (err) {
+                                        return reject(err);
+                                    }
+                                    resolve(result);
+                                });
+                        });
+                    }
+                };
+
+            });
         })
         .then(function() {
 
@@ -97,5 +113,6 @@ module.exports = function(app) {
             return email;
 
         });
+
 
 };
