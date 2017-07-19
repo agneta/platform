93fa5944a3964be0b24e1579f7bd0cc9654bb509
Raphael Varonos <r.varonos@gmail.com>
2017-07-19 23:05:22 +0300

    Update license header

--- scripts/postinstall/git/init.js
+++ scripts/postinstall/git/init.js
@@ -1,6 +1,6 @@
 /*   Copyright 2017 Agneta Network Applications, LLC.
  *
- *   Source file: hooks/preinstall/git/init.js
+ *   Source file: scripts/postinstall/git/init.js
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
