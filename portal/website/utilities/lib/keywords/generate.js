0ccae58475ced2284ae8127606dd583d59d311f7
Raphael Varonos <r.varonos@gmail.com>
2017-08-07 20:19:43 +0300

    Update license header

--- portal/website/utilities/lib/keywords/generate.js
+++ portal/website/utilities/lib/keywords/generate.js
@@ -1,6 +1,6 @@
 /*   Copyright 2017 Agneta Network Applications, LLC.
  *
- *   Source file: portal/website/utilities/search/lib/generate.js
+ *   Source file: portal/website/utilities/lib/keywords/generate.js
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,6 +1,6 @@
 /*   Copyright 2017 Agneta Network Applications, LLC.
  *
- *   Source file: .eslintrc.js
+ *   Source file: test/.eslintrc.js
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
