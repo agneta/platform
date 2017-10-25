3609f53284893102f3d3434574cb0306d68a09a4
Raphael Varonos <r.varonos@gmail.com>
2017-10-25 11:20:47 +0300

    lock menu and improve has-role ui

--- theme/source/main/account/directives.js
+++ theme/source/main/account/directives.js
@@ -6,28 +6,42 @@ function _e_directives(app) {
       scope: true,
       link: function(scope, element, attrs) {
 
-        element.addClass('has-role');
+        var parent = element.parent();
+        parent.addClass('has-role');
 
-        var roles = JSON.parse(attrs.hasRole) || [];
-        var account = $rootScope.account;
-        var hasRoles = [];
+        function check() {
 
-        for (var index in roles) {
-
-          var role = roles[index];
-          var hasRole = account[role];
-          hasRole = hasRole && hasRole.id;
-
-          if (hasRole) {
-            hasRoles.push(role);
+          var account = $rootScope.account;
+          if(!account){
+            return;
           }
+
+          var roles = JSON.parse(attrs.hasRole) || [];
+          var hasRoles = [];
+
+          for (var index in roles) {
+
+            var role = roles[index];
+            var hasRole = account[role];
+            hasRole = hasRole && hasRole.id;
+
+            if (hasRole) {
+              hasRoles.push(role);
+            }
+          }
+
+          if (hasRoles.length) {
+            parent.removeClass('has-no-role');
+          } else {
+            parent.addClass('has-no-role');
+          }
+
         }
 
-        if (hasRoles.length) {
-          element.removeClass('has-no-role');
-        } else {
-          element.addClass('has-no-role');
-        }
+        check();
+
+        $rootScope.$on('accountCheck',check);
+
 
       }
     };
