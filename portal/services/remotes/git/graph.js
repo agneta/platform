a651d663d9e5eb44081d17862722b79fb8a83bbf
Raphael Varonos <r.varonos@gmail.com>
2017-10-30 18:36:42 +0200

    display git commit status on the server

--- portal/services/remotes/git/graph.js
+++ portal/services/remotes/git/graph.js
@@ -5,29 +5,34 @@ module.exports = function(Model, app) {
   var config = app.get('git');
 
   Model.graph = function() {
-    console.log(app.git.branch);
 
     var branchName = app.git.branch.current;
     var remoteBranch = `${config.remote.name}/${branchName}`;
     var behind;
     var ahead;
+    var remoteLog;
 
     return Promise.resolve()
       .then(function() {
-        return app.git.native.raw(['rev-list','--left-right','--count',`${remoteBranch}...${branchName}`]);
+        return app.git.native.raw(['rev-list', '--left-right', '--count', `${remoteBranch}...${branchName}`]);
       })
       .then(function(commitCount) {
         commitCount = S(commitCount).strip('\n').s.split('\t');
         behind = parseFloat(commitCount[0]);
         ahead = parseFloat(commitCount[1]);
         return app.git.native.log(['--max-count', '3', '--remotes', remoteBranch]);
-
       })
-      .then(function(remoteLog) {
-        return{
+      .then(function(_remoteLog) {
+        remoteLog = _remoteLog;
+        return app.git.native.log(['-1']);
+      })
+      .then(function(localLog) {
+        return {
+          branch: branchName,
           behind: behind,
           ahead: ahead,
-          shortLog: remoteLog.all
+          shortLog: remoteLog.all,
+          current: localLog.latest
         };
       });
   };
--- portal/website/source/system/server.js
+++ portal/website/source/system/server.js
@@ -2,12 +2,31 @@
 
   var app = angular.module('MainApp');
 
-  app.controller('SystemServerCtrl', function($scope, GIT) {
-    GIT.status()
-      .$promise
-      .then(function(result) {
-        console.log(result);
-      });
+  app.controller('SystemServerCtrl', function($scope,$rootScope, GIT) {
+
+    function check(){
+      GIT.graph()
+        .$promise
+        .then(function(result) {
+          console.log(result);
+          $scope.graph = result;
+        });
+    }
+    check();
+
+    $scope.fetch = function() {
+      $rootScope.loadingMain = true;
+      GIT.fetch()
+        .$promise
+        .then(function() {
+          check();
+        })
+        .finally(function() {
+          $rootScope.loadingMain = false;
+        });
+    };
+
+
   });
 
 
