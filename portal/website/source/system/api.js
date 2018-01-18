1503794de3de992eeeed24409e40b18310769258
Raphael Varonos <r.varonos@agneta.io>
2018-01-18 14:49:22 +0100

    Displaying remote methods in the page

--- portal/website/source/system/api.js
+++ portal/website/source/system/api.js
@@ -3,6 +3,7 @@ agneta.directive('AgSystemApi',function(API){
   var vm = this;
 
   vm.model = {};
+  vm.method = {};
 
   API.models({
   })
@@ -12,15 +13,20 @@ agneta.directive('AgSystemApi',function(API){
     });
 
   vm.model.select = function(model){
-    vm.model.selected = model.name;
+    vm.model.selected = model;
 
     API.methods({
       name: model.name
     })
       .$promise
       .then(function(result) {
-        console.log(result);
+        vm.method.list = result.list;
       });
   };
+
+  vm.method.select = function(method){
+    vm.method.selected = method;
+  };
+
 
 });
