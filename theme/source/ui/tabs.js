1e839a7d8ae0378f482003c757b3a3640d7049a4
Raphael Varonos <r.varonos@agneta.io>
2018-04-03 12:13:48 +0200

    Improve tab navigation with history push

--- theme/source/ui/tabs.js
+++ theme/source/ui/tabs.js
@@ -1,21 +1,37 @@
 agneta.directive('AgTabs',function($routeParams,$location, $timeout){
   var vm = this;
 
-  vm.select = function(name) {
+  vm.select = function(name,noHistory) {
     vm.activeSection = name;
     var query = $location.search();
     query.tab = name;
-    $location.search(query);
+    var location = $location.search(query);
+    if(noHistory){
+      location.replace();
+    }
     $timeout(function(){
       vm.$emit('tab-change',name);
     },100);
   };
 
   if($routeParams.tab){
-    vm.select($routeParams.tab);
+    vm.select($routeParams.tab,true);
   }
   else if(vm.activeSection){
-    vm.select(vm.activeSection);
+    vm.select(vm.activeSection,true);
   }
+
+  vm.$on('$routeUpdate', function() {
+    var check = $location.search();
+    if(!check.tab){
+      return;
+    }
+    if (
+      check.tab == vm.activeSection
+    ) {
+      return;
+    }
+    vm.select(check.tab,true);
+  });
 
 });
