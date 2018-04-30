32c9a924e6f57eb938e7dfb455174cdfb7c283c2
Raphael Varonos <r.varonos@agneta.io>
2018-04-30 10:43:45 +0200

    Isolate scope for ui.tabs

--- theme/website/source/ui/tabs.js
+++ theme/website/source/ui/tabs.js
@@ -1,43 +1,46 @@
-agneta.directive('AgTabs',function($routeParams,$location,$attrs, $timeout){
+var app = window.angular.module('MainApp');
 
-  var vm = this;
-  var tabs = vm.tabs = vm.tabs || {};
-  var queryName = $attrs.name || 'tab';
-  var tab = tabs[queryName] = {};
+app.directive('agTabs', function($routeParams,$location, $timeout) {
+  return {
+    restrict: 'A',
+    scope: true,
+    link: function(vm, element, attrs) {
+      var queryName = attrs.name || 'tab';
 
-  tab.select = function(name,noHistory) {
-    //console.log(name);
-    tab.activeSection = name;
-    var query = $location.search();
-    query[queryName] = name;
-    var location = $location.search(query);
-    if(noHistory){
-      location.replace();
+      vm.select = function(name,noHistory) {
+        //console.log(name);
+        vm.activeSection = name;
+        var query = $location.search();
+        query[queryName] = name;
+        var location = $location.search(query);
+        if(noHistory){
+          location.replace();
+        }
+        $timeout(function(){
+          vm.$emit('tab-change',name);
+        },100);
+      };
+
+      if($routeParams[queryName]){
+        vm.select($routeParams[queryName],true);
+      }
+      else if(attrs.default){
+        vm.select(attrs.default,true);
+      }
+
+
+      vm.$on('$routeUpdate', function() {
+        var check = $location.search();
+        if(!check[queryName]){
+          return;
+        }
+        if (
+          check[queryName] == vm.activeSection
+        ) {
+          return;
+        }
+        vm.select(check[queryName],true);
+      });
     }
-    $timeout(function(){
-      vm.$emit('tab-change',name);
-    },100);
   };
-
-  if($routeParams[queryName]){
-    tab.select($routeParams[queryName],true);
-  }
-  else if($attrs.default){
-    tab.select($attrs.default,true);
-  }
-
-
-  vm.$on('$routeUpdate', function() {
-    var check = $location.search();
-    if(!check[queryName]){
-      return;
-    }
-    if (
-      check[queryName] == tab.activeSection
-    ) {
-      return;
-    }
-    tab.select(check[queryName],true);
-  });
-
 });
