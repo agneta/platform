9364b03b06b91b64f786d0439a32b08bb018606f
Raphael Varonos <r.varonos@agneta.io>
2018-10-27 16:51:51 +0200

    get fixer

--- services/models/media/get.js
+++ services/models/media/get.js
@@ -1,48 +1,42 @@
-module.exports = function(Model,app) {
-
+module.exports = function(Model, app) {
   Model.__get = function(options) {
+    return Promise.resolve().then(function() {
+      var promise;
+      var id = options.id;
+      var location = options.location;
+      if (id) {
+        promise = Model.findById(id, {
+          fields: options.fields
+        });
+      }
 
-    return Promise.resolve()
-      .then(function() {
-        var promise;
-        var id = options.id;
-        var location = options.location;
+      if (location) {
+        promise = Model.findOne({
+          where: {
+            location: app.helpers.normalizePath(location)
+          },
+          fields: options.fields
+        });
+      }
 
-        if (id) {
-          promise = Model.findById(id);
-        }
+      if (!promise) {
+        throw new Error('Must provide the file id or location');
+      }
 
-        if (location) {
-          promise = Model.findOne({
-            where: {
-              location: app.helpers.normalizePath(location)
-            }
-          });
-        }
-
-        if (!promise) {
-          throw new Error('Must provide the file id or location');
-        }
-
-        return promise;
-      });
-
+      return promise;
+    });
   };
 
-  Model.get = function(options){
+  Model.get = function(options) {
+    return Model.__get(options).then(function(object) {
+      if (!object) {
+        return Promise.reject({
+          statusCode: 400,
+          message: 'Media file not found'
+        });
+      }
 
-    return Model.__get(options)
-      .then(function(object) {
-
-        if (!object) {
-          return Promise.reject({
-            statusCode: 400,
-            message: 'Media file not found'
-          });
-        }
-
-        return object;
-      });
-
+      return object;
+    });
   };
 };
